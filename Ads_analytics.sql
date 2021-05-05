9782217852510dcb66f55186cbccd8568061fc2c
Tatiana Linnik <tanitachka1702@gmail.com>
2021-05-05 19:34:21 +0100

    Update and rename Ads_analytics.txt to Ads_analytics.sql

--- Ads_analytics.sql
+++ Ads_analytics.sql
@@ -1,62 +1,62 @@
 -- 1. посчитаем число всех событий по дням, число показов, число кликов, число уникальных объявлений и уникальных кампаний
 --(поменяла везде case when на countif)
--- SELECT date,countIf(event = 'view') AS view_count,
---        countIf(event = 'click') AS click_count,
---        COUNT(distinct ad_id) AS ads_count,
---        COUNT(distinct campaign_union_id) AS camp_count
--- from ads_data group by date;
+SELECT date,countIf(event = 'view') AS view_count,
+       countIf(event = 'click') AS click_count,
+       COUNT(distinct ad_id) AS ads_count,
+       COUNT(distinct campaign_union_id) AS camp_count
+from ads_data group by date;
 --
 -- 2. 2019-04-05 сильно увеличилось количество кликов и просмотров, потому что появилась реклама 112583 (campaign = 112260)
--- SELECT ad_id,date,campaign_union_id,
---        countIf(event = 'view') AS view_count,
---        countIf(event = 'click') AS click_count
--- FROM ads_data
--- GROUP BY ad_id,date,campaign_union_id
--- ORDER BY click_count DESC;
+SELECT ad_id,date,campaign_union_id,
+       countIf(event = 'view') AS view_count,
+       countIf(event = 'click') AS click_count
+FROM ads_data
+GROUP BY ad_id,date,campaign_union_id
+ORDER BY click_count DESC;
 --
 -- 2a) проверили встречался ли этот id ранее 2019-04-05
--- SELECT distinct date
--- FROM ads_data
--- WHERE ad_id = 112583 or campaign_union_id = 112260;
+SELECT distinct date
+FROM ads_data
+WHERE ad_id = 112583 or campaign_union_id = 112260;
 --
 -- 3. топ 10 объявлений по CTR за все время (сделала limit 20 , потому что первые 10 надо как-то удалить=) )
 --(Добавила HAVING)
--- SELECT ad_id,
---        round(countIf(event = 'click')/countIf(event = 'view'),2) as ctr
--- FROM ads_data
--- GROUP BY ad_id
--- HAVING countIf(event = 'view')>0
--- ORDER BY ctr DESC LIMIT 10;
+SELECT ad_id,
+       round(countIf(event = 'click')/countIf(event = 'view'),2) as ctr
+FROM ads_data
+GROUP BY ad_id
+HAVING countIf(event = 'view')>0
+ORDER BY ctr DESC LIMIT 10;
 --
 -- 4. объявления без показов, но с кликами - 9 штук, проблема на всех платформах, наличие видео не влияет
--- SELECT ad_id,platform,has_video
--- FROM ads_data
--- GROUP BY ad_id,platform, has_video
--- HAVING countIf(event = 'view') = 0
--- ORDER BY ad_id;
+SELECT ad_id,platform,has_video
+FROM ads_data
+GROUP BY ad_id,platform, has_video
+HAVING countIf(event = 'view') = 0
+ORDER BY ad_id;
 
 -- 5.1) Есть ли различия в CTR у объявлений с видео и без?
--- SELECT has_video,
---        round(countIf(event = 'click')/countIf(event = 'view'),2) AS ctr
--- FROM ads_data
--- GROUP BY has_video;
+SELECT has_video,
+       round(countIf(event = 'click')/countIf(event = 'view'),2) AS ctr
+FROM ads_data
+GROUP BY has_video;
 --
 -- 5.2) Чему равняется 95 процентиль CTR по всем объявлениям за 2019-04-04? 0.08
--- SELECT quantile(0.95)(ctr) AS procentil_95
--- FROM (SELECT ad_id,
---         round(countIf(event = 'click')/countIf(event = 'view'),2) AS ctr
---         FROM ads_data
---         WHERE date = '2019-04-04'
---         GROUP BY ad_id);
+SELECT quantile(0.95)(ctr) AS procentil_95
+FROM (SELECT ad_id,
+       round(countIf(event = 'click')/countIf(event = 'view'),2) AS ctr
+       FROM ads_data
+       WHERE date = '2019-04-04'
+       GROUP BY ad_id);
 --
 -- 6. (поменяла на multiIF)
 -- заработок по дням больше всего за 5 апреля, меньше всего за 1 апреля
--- SELECT date,
--- round(sum(multiIf((ad_cost_type = 'CPC') AND (event = 'click'),
---         ad_cost, (ad_cost_type = 'CPM') AND (event = 'view'), ad_cost / 1000, 0)),2) AS income
--- FROM ads_data
--- GROUP BY date
--- ORDER BY income;
+SELECT date,
+round(sum(multiIf((ad_cost_type = 'CPC') AND (event = 'click'),
+      ad_cost, (ad_cost_type = 'CPM') AND (event = 'view'), ad_cost / 1000, 0)),2) AS income
+FROM ads_data
+GROUP BY date
+ORDER BY income;
 
 --Предыдущая версия
 --заработок по дням за рекламу CPM
